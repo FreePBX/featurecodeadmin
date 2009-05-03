e203c462a89e405504434299877ca859a120bb46
Micke Carlsson <mickecamino@gmail.com>
2009-05-03 09:02:25 +0000

    Re #3495 adds license text to varios files in featurecodeadmin

--- page.featurecodeadmin.php
+++ page.featurecodeadmin.php
@@ -1,16 +1,21 @@
 <?php 
-// Original Copyright (C) 2006 Niklas Larsson
-// Re-written 20060331, Rob Thomas <xrobau@gmail.com>
+// This file is part of FreePBX.
 //
-//This program is free software; you can redistribute it and/or
-//modify it under the terms of the GNU General Public License
-//as published by the Free Software Foundation; either version 2
-//of the License, or (at your option) any later version.
+//    FreePBX is free software: you can redistribute it and/or modify
+//    it under the terms of the GNU General Public License as published by
+//    the Free Software Foundation, either version 2 of the License, or
+//    (at your option) any later version.
 //
-//This program is distributed in the hope that it will be useful,
-//but WITHOUT ANY WARRANTY; without even the implied warranty of
-//MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-//GNU General Public License for more details.
+//    FreePBX is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+//    GNU General Public License for more details.
+//
+//    You should have received a copy of the GNU General Public License
+//    along with FreePBX.  If not, see <http://www.gnu.org/licenses/>.
+//    Copyright (C) 2006 Niklas Larsson
+//    Copyright (C) 2006 Rob Thomas <xrobau@gmail.com>
+//
 
 $action = isset($_REQUEST['action'])?$_REQUEST['action']:'';
 $dispnum = "featurecodeadmin"; //used for switch on config.php
