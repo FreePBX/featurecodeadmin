7219aa6574fd00216feef3cc9d6ff551de522235
Philippe Lindheimer <philippe.lindheimer@schmoozecom.com>
2012-01-10 15:28:04 +0000

    revert r13179 re #5506 this was just a missing class in framework

--- page.featurecodeadmin.php
+++ page.featurecodeadmin.php
@@ -91,13 +91,13 @@ $featurecodes = featurecodes_getAllFeaturesDetailed();
 	$currentmodule = "(none)";
 	foreach($featurecodes as $item) {
 
-		$moduledesc = isset($item['moduledescription']) ? _($item['moduledescription'], $item['modulename']) : null;
+		$moduledesc = isset($item['moduledescription']) ? modgettext::_($item['moduledescription'], $item['modulename']) : null;
 		// just in case the translator put the translation in featurcodes module:
 		if (($moduledesc !== null) && ($moduledesc == $item['moduledescription'])) {
 			$moduledesc = _($moduledesc);
 		}
 
-		$featuredesc = _($item['featuredescription'], $item['modulename']);
+		$featuredesc = modgettext::_($item['featuredescription'], $item['modulename']);
 		// just in case the translator put the translation in featurcodes module:
 		if ($featuredesc == $item['featuredescription']) {
 			$featuredesc = _($featuredesc);
