8ba934356b0e4b11a673d513e38bc5e49c971a46
Andrew Nagy <andrew.nagy@the159.com>
2015-05-21 18:07:51 -0400

    visual tweaks and styling fixes

--- views/main.php
+++ views/main.php
@@ -38,11 +38,14 @@
 										<div class="element-container">
 											<div class="row">
 												<div class="form-group">
-													<div class="col-md-6 control-label">
-														<?php echo $item['title']?>
+													<div class="col-md-6">
+														<label class="control-label" for="<?php echo $item['feature']?>"><?php echo $item['title']?></label>
+														<?php if(!empty($item['help'])) {?>
+															<i class="fa fa-question-circle fpbx-help-icon" data-for="<?php echo $item['feature']?>"></i>
+														<?php } ?>
 													</div>
 													<div class="col-md-2">
-														<input type="text" name="fc[<?php echo $item['module']?>][<?php echo $item['feature']?>][code]" value="<?php echo $item['code']?>" id="custom_<?php echo $item['id']?>" data-default="<?php echo $item['default']?>" data-custom="<?php echo $item['custom']?>" class="form-control code" <?php echo (!$item['iscustom']) ? 'readonly' : ''?> required pattern="\*{0,2}[0-9]{0,5}">
+														<input type="text" name="fc[<?php echo $item['module']?>][<?php echo $item['feature']?>][code]" value="<?php echo $item['code']?>" id="custom_<?php echo $item['id']?>" data-default="<?php echo $item['default']?>" placeholder="<?php echo $item['default']?>" data-custom="<?php echo $item['custom']?>" class="form-control code" <?php echo (!$item['iscustom']) ? 'readonly' : ''?> required pattern="\*{0,2}[0-9]{0,5}">
 													</div>
 													<div class="col-md-4">
 														<span class="radioset">
@@ -54,6 +57,11 @@
 															<label for="ena_<?php echo $item['id']?>">Enabled</label>
 														</span>
 													</div>
+												</div>
+											</div>
+											<div class="row">
+												<div class="col-md-12">
+													<span id="<?php echo $item['feature']?>-help" class="help-block fpbx-help-block"><?php echo $item['help']?></span>
 												</div>
 											</div>
 										</div>
